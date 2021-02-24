50460ed39887985ffeab9ae82d8ef181dc4e6590
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-02-24 11:10:25 -0600

    us 6

--- src/test/java/com/Library_ct/pages/LoginPage.java
+++ src/test/java/com/Library_ct/pages/LoginPage.java
@@ -1,20 +1,20 @@
-package com.Library_ct.pages;
+    package com.Library_ct.pages;
 
-import com.Library_ct.utilities.Driver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+    import com.Library_ct.utilities.Driver;
+    import org.openqa.selenium.WebElement;
+    import org.openqa.selenium.support.FindBy;
+    import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
-    public LoginPage(){
-        PageFactory.initElements(Driver.getDriver(), this);
+    public class LoginPage {
+        public LoginPage(){
+            PageFactory.initElements(Driver.getDriver(), this);
+        }
+        @FindBy(id = "inputEmail")
+        public WebElement userNameInput;
+
+        @FindBy (id = "inputPassword")
+        public WebElement passwordInput;
+
+        @FindBy (xpath="//button[.='Sign in']")
+        public WebElement sighInButton;
     }
-    @FindBy(id = "inputEmail")
-    public WebElement userNameInput;
-
-    @FindBy (id = "inputPassword")
-    public WebElement passwordInput;
-
-    @FindBy (xpath="//button[.='Sign in']")
-    public WebElement sighInButton;
-}
--- src/test/resources/features/US_6_TableNames.feature
+++ src/test/resources/features/US_6_TableNames.feature
@@ -1,13 +1,13 @@
-Feature:As a students, I should be able to see tables with default info
-  @tableColumns
-  Scenario: Table columns names
-    Given the user is on the homepage
-    Then the user should see the following column names:
-      | Actions     |
-      | ISBN        |
-      | Name        |
-      | Author      |
-      | Category    |
-      | Year        |
-      | Borrowed By |
+  Feature:As a students, I should be able to see tables with default info
+    @tableColumns
+    Scenario: Table columns names
+      Given the user is on the homepage
+      Then the user should see the following column names:
+        | Actions     |
+        | ISBN        |
+        | Name        |
+        | Author      |
+        | Category    |
+        | Year        |
+        | Borrowed By |
 
